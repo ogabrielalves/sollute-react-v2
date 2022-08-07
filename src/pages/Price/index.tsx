8bd5a408d6c9d189d64cd9817d7e120d486bad9e
Gabriel <gabriel.silva@bandtec.com.br>
2022-08-07 20:16:47 -0300

    Criação de botão de retorno

--- src/pages/Price/index.tsx
+++ src/pages/Price/index.tsx
@@ -1,10 +1,20 @@
 import style from './Price.module.scss';
+
 import PriceCard from '../../components/PriceCard';
 import PictureMonthly from '../../assets/img/PictureMonthly.svg';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+
 
 function Price() {
     return (
         <div className={style.background}>
+            <Typography variant="h3" sx={{ fontWeight: '500' }}>
+                ESCOLHA SEU PLANO
+            </Typography>
+
             <div className={style.boxPrice}>
                 <div className={style.priceCard}>
                     <PriceCard
@@ -15,11 +25,11 @@ function Price() {
                         variantButton='outlined'
                         backgroundColor='transparent'
                         borderColor='#3E00FF'
-                        colorText= '#3E00FF'
+                        colorText='#3E00FF'
                     />
                 </div>
 
-                <div className={style.priceCard}>
+                <div className={style.priceCardCenter}>
                     <PriceCard
                         typePlan='SEMESTRAL'
                         pricePlan={135}
@@ -28,7 +38,7 @@ function Price() {
                         variantButton='contained'
                         backgroundColor='#784DFF'
                         borderColor='#784DFF'
-                        colorText= '#fff'
+                        colorText='#fff'
                     />
                 </div>
 
@@ -41,10 +51,13 @@ function Price() {
                         variantButton='outlined'
                         backgroundColor='transparent'
                         borderColor='#9F80FF'
-                        colorText= '#9F80FF'
+                        colorText='#9F80FF'
                     />
                 </div>
             </div>
+            <Button variant="contained" startIcon={<ArrowBackIcon/>} sx={{backgroundColor: '#3E00FF', height:30, width:230}}>
+                VOLTAR AO INICIO
+            </Button>
         </div>
     );
 }
